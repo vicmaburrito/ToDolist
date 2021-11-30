b59a49aa0be6130b74091f13160f3e138486f0f7
vicmaburrito <vmaldaraca@gmail.com>
2021-11-30 16:27:02 -0600

    Create constructor and create object Todos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
+
+class Todo {
+    constructor(description,completed,index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+    }
+}
+
+const Todos = [
+    new Todo('Wash the dishes', true, 0),
+    new Todo('Complete To Do list project', false, 1)
+];
