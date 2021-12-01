90b206513ce1ee178618c8e31466d6c63d475c7b
vicmaburrito <vmaldaraca@gmail.com>
2021-11-30 23:52:47 -0600

    Create displayTodos function to display data iterating an array with forEach loop

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import './style.css';
 
 class Todo {
     constructor(description,completed,index) {
@@ -8,7 +9,24 @@ class Todo {
     }
 }
 
-const Todos = [
+const toDos = [
     new Todo('Wash the dishes', true, 0),
     new Todo('Complete To Do list project', false, 1)
 ];
+
+function displayTodos() {
+    const ul = document.getElementById('toDoList');
+    ul.classList.add('list');
+    toDos.forEach((element) => {
+        const li = document.createElement('li')
+        li.classList.add('list-item');
+        li.innerHTML = ` 
+        ${element.description}
+        `
+        ul.appendChild(li)
+    });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    displayTodos();
+  });
