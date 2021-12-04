f7e43dcb835625f293cc447951e0c627f3b58dcb
vicmaburrito <vmaldaraca@gmail.com>
2021-12-03 18:33:58 -0600

    Add destroy To-do function

--- src/todos_controller.js
+++ src/todos_controller.js
@@ -7,4 +7,14 @@ function updateTodo(newTodo, oldTodo) {
   return oldTodo;
 }
 
-export { createTodo, destroyTodo, updateTodo }
+function destroyTodo(todo, todoList) {
+  const index = todoList.indexOf(todo);
+  todoList.splice(index, 1);
+  todoList.forEach((todo, currentIndex) => {
+    if (currentIndex >= index) {
+      todo.index -= 1;
+    }
+  });
+}
+
+export { createTodo, destroyTodo, updateTodo };
